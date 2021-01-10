987ee4c702988c70b0d08ea692a6add5e429e760
Mahsan-code <mpoorasadollahi@gmail.com>
2021-01-10 00:22:06 -0500

    mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,14 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-const result = head([4,5,9,3,6]);
-assertEqual(result, 4);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it ('return 4 for [4,5,9,3,6]', ()=>{
+    assert.strictEqual(head([4,5,9,3,6]), 4);
+  });
+});
